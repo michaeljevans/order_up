2d0d55699209a1c8b6eeff9e136b4179c1fda09c
Michael Evans <michaeljevans113@gmail.com>
2020-07-24 10:35:21 -0600

    Add route for chef ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/dishes/:id', to: 'dishes#show'
+
+  get '/chefs/:id',                  to: 'chefs#show'
+  get '/chefs/:chef_id/ingredients', to: 'chefs#ingredients'
 end
