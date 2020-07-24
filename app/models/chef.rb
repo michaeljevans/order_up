531930f9bc581e4c180e8e9921144319f138c9a9
Michael Evans <michaeljevans113@gmail.com>
2020-07-24 10:38:28 -0600

    Add #unique_ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef < ApplicationRecord
   validates_presence_of :name
 
   has_many :dishes
+
+  def unique_ingredients
+    dishes.select('ingredients.name').joins(:ingredients).distinct
+  end
 end
